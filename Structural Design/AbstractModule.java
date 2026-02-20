e161099da43e9e7de6d1a287f4cd3b32c5306a0d
Saif-187 <143109894+Saif-187@users.noreply.github.com>
2026-02-20 21:55:21 +0600

    Add files via upload

--- Structural Design/AbstractModule.java
+++ Structural Design/AbstractModule.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 public abstract class AbstractModule extends AbstractProduct {
     public AbstractModule(String name) {
         super(name);
@@ -14,18 +13,5 @@ public abstract class AbstractModule extends AbstractProduct {
             System.out.println();
         }
     }
-    public void addCourse(Product product) {
-        if(product instanceof Course) {
-            addProduct(product);
-        }
-        else{
-            throw new IllegalArgumentException("Only Course components can be added to a Module.");
-        }
-    }
-    public ArrayList<Product> getCourses() {
-        return products;
-    }
-    public void removeCourse(Product product) {
-        removeProduct(product);
-    }
+    
 }
--- Structural Design/Module.java
+++ Structural Design/Module.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+
 public class Module extends AbstractModule {
     public Module(String name) {
         super(name);
+    }
+    public void addCourse(Product product) {
+        if(product instanceof Course) {
+            addProduct(product);
+        }
+        else{
+            throw new IllegalArgumentException("Only Course components can be added to a Module.");
+        }
+    }
+    public ArrayList<Product> getCourses() {
+        return products;
+    }
+    public void removeCourse(Product product) {
+        removeProduct(product);
     }
 }
